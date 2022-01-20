84beebd48276ce29f41a00c1ebdd529452af90c6
Susan-Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-01-20 23:12:22 +0530

    python

---
+++ faulty calculator.py
@@ -0,0 +1,26 @@
+                                                  faulty calculator
+
+print("enter the opeartor: + - / *")
+operator=input()
+print("enter the numbers")
+a=int(input(print("a")))
+b=int(input(print("b")))
+if operator== '+' :
+     if a==56 and b==9 :
+         print("result is 77")
+     else  :
+         print( "is:" ,a+b)
+elif operator== '*' :
+     if a==45 and b==3 :
+         print("result is 555")
+     else  :
+         print( "is:" ,a*b)
+elif operator== '/' :
+     if a==56 and b==6 :
+         print("result is 4")
+     else  :
+         print( "is:", a/b)
+elif operator== '-' :
+     print( "is:" ,a-b) 
+else :
+    print("invalid")    
