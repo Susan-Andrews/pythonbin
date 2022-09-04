4a6f416bb3ca76d01a5a833a22c32dc5ed72a19f
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-09-04 21:01:35 +0530

    Create tryy.py

---
+++ tryy.py
@@ -0,0 +1,19 @@
+#code 
+# change the settings to run in terminal check box in vscode
+# comma gives space
+x=input('enter your name:')
+print('hi'+x)
+
+
+n=5
+print(str(n)+"my fav number")        # convert number to string
+
+               # try and except
+try:
+    number=int(input("enter any number:"))
+    print (number)
+except:
+    print("invalid input")   # instead of throwing traceback error it shows a beautiful error.so we use except blocks
+
+#can add many except 
+
