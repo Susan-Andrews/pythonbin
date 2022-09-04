ed0f6efd5f835e7c7d61cfeea089e861780aa4df
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-09-04 21:17:13 +0530

    Update online library management system.py

--- online library management system.py
+++ online library management system.py
@@ -1,3 +1,18 @@
+
+# Create a library class
+# display book
+# lend book - (who owns the book if not present)
+# add book
+# return book
+
+# HarryLibrary = Library(listofbooks, library_name)
+
+
+#dictionary (books-nameofperson)
+
+# create a main function and run an infinite while loop asking
+# users for their input
+
 class Library :
     def __init__(self,list,name): #constructor
         self.booksList=list
