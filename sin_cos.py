3db29cc1ec93f42f068eabf199d1664dc61cf8dc
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-12-07 22:34:51 +0530

    Create sin_cos.py

---
+++ sin_cos.py
@@ -0,0 +1,6 @@
+import math 
+n=int(input("Enter the number to which cosine and sine to be calculated:"))
+sinn=math.sin(n*3.14/180)
+cosn=math.cos(n*3.14/180)
+print("Sin n =",sinn)
+print("Cos n =",cosn)
