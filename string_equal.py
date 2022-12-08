6f9ceb83ed0a3589d4f015e4d046349dd3a76bf6
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-12-08 19:00:29 +0530

    Create string_equal.py

---
+++ string_equal.py
@@ -0,0 +1,7 @@
+import string 
+string1=str(input("Enter first string: "))
+string2=str(input("Enter second string: "))
+if string1==string2 :
+    print("The string is same!")
+else:
+    print("The string is not same!")
