e50820e2852592f34334543a90dc6edf90873440
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-12-10 21:39:57 +0530

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,6 +1,6 @@
 #METHOD 1
-num =input("enter a string which is to be reversed : ")
-print(str(num)[::-1])
+str_rev =input("enter a string which is to be reversed : ")
+print(str(str_rev)[::-1])
 
 
 
@@ -8,12 +8,12 @@ print(str(num)[::-1])
 
 
 #METHOD 2
-num = input("enter a string which is to be reversed : ")
+str_rev= input("enter a string which is to be reversed : ")
 reversed_num = 0
 
-while num != 0:
-    digit = num % 10
+while str_rev != 0:
+    digit = str_rev % 10
     reversed_num = reversed_num * 10 + digit
-    num //= 10
+    str_rev //= 10
 
 print("Reversed Number: " + str(reversed_num))
