ab00a56f560fdbc530e51536abd9a078053fee1f
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-12-13 22:35:16 +0530

    Update Seq1.py

--- Seq1.py
+++ Seq1.py
@@ -4,7 +4,7 @@ n=int(input("Enter the limit of the series  1+ 1/2 + 1/3 + 1/4 + ....  + 1/n  : 
 sum=1.0
 i=2.0
 while(i<=n):
-    sum=sum+1.0/round(i,2)
+    sum=sum+1.0/i
     i=i+1
     
     print(Fraction(sum))
