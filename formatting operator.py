7dc9c7421d3a6d3bb5c4e768f51fd50c84a8285d
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2023-01-02 23:45:46 +0530

    Add files via upload

---
+++ formatting operator.py
@@ -0,0 +1,7 @@
+# its a small code snippet whic actually describes about using formatting operator similar in c language 
+camels = 42
+print('I have spotted %d camels.' % camels)
+print('In %d years I have spotted %g %s.' % (3, 0.1, 'camels'))
+print(" ")
+
+print('My name %s and i am %d years old' % ("Susan", 21))
