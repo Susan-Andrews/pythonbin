2cb841647f2d0e0221ec27d2b3302906acbe6345
Susan Andrews <susanandrews2001@gmail.com>
2023-04-02 22:46:28 +0530

    collections

---
+++ Elective/collection/set,dictionary.py
@@ -0,0 +1,19 @@
+#Program to define and process set
+myset={"banana","cherry","apple", 45, 7.2}
+print(type(myset))
+for x in myset:
+    print(x)
+print("banana" in myset)
+
+'''
+output
+
+    <class 'set'>
+    banana
+    apple
+    7.2
+    cherry
+    45
+    True
+
+'''
---
+++ Elective/collection/set,dictionary2.py
@@ -0,0 +1,18 @@
+
+# Program to define and process dictionary
+thisdict ={"brand":"Ford","model":"Mustang","year":1967}
+print(thisdict)
+print(thisdict["brand"])
+x = thisdict["model"]
+print (x)
+y=thisdict.get("model")
+print
+'''
+
+output
+
+{'brand': 'Ford', 'model': 'Mustang', 'year': 1967}
+Ford
+Mustang
+
+'''
